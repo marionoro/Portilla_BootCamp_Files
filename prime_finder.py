42a18a6feb0d3101b29f21061bf6cb73148d9b67
marionoro <54335685+marionoro@users.noreply.github.com>
2019-09-12 15:41:58 -0500

    Next Prime Number
     Have the program find prime numbers until the user chooses to stop asking for the next one.

---
+++ prime_finder.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 11:38:54 2019
+
+@author: e128068
+"""
+
+def find_prime():
+    primes = []
+    for x in range(2, 2**100):
+        is_prime = True
+        for y in primes:
+            if y > (x*0.5):
+                break
+            if x % y == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(x)
+            yield x
+
+
+if __name__ == '__main__':
+    prime_finder = find_prime()
+    print('Welcome to Prime Finder!')
+    print(f'The first prime number is {next(prime_finder)}')
+    answer = input("Would you like to see the next prime number? (type 'y' if so) ")
+    while answer == 'y' or answer == 'yes':
+        print(f'The next prime number is {next(prime_finder)}')
+        answer = input("Would you like to see the next prime number? (type 'y' if so) ")
+    print('I hope you had fun!')
+        
+
+
